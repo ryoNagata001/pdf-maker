213984e90598f400fe78193fb0210ccb464dd301
ryo nagata <ryo.nagata.utokyo@gmail.com>
2018-02-23 21:59:23 +0900

    push for sharing

--- queue/ad_salary/pdf-maker/src/components/HelloWorld.vue
+++ queue/ad_salary/pdf-maker/src/components/HelloWorld.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="hello">
     <button @click="createPdf()">click</button>
+    <button @click="printPDF()">canvas</button>
+    <div id="pdf">
+      <h1 style="text-align: center;">hello world</h1>
+      <h2 style="color: red;">Can I type in my long words hogehogehgoeghoegheogehogegheoghoehgoehogheogheohgoehgoe</h2>
+    </div>
   </div>
 </template>
 
 <script>
 import JsPDF from 'jspdf'
 import 'jspdf-autotable'
+// import html2canvas from 'html2canvas'
 
 export default {
+  data () {
+    return {
+      canvasData: null
+    }
+  },
   methods: {
     createPdf () {
       var columns = ['ID', 'Name', 'Country']
@@ -18,14 +29,25 @@ export default {
         [3, 'Garcia', 'Madagascar']
       ]
       let pdfName = 'test'
-      var doc = new JsPDF('p', 'pt')
+      var doc = new JsPDF()
       var fontSize = 30
       doc.setFontSize(fontSize)
-      var xOffset = (doc.internal.pageSize.width / 2) - (doc.getStringUnitWidth('Hello World') * fontSize / 2)
-      doc.text('Hello World', xOffset, 30)
+      var text = 'hogehogeoghoehogheohgoehogheohgoehogheohgoehgoheohgoehgoeh'
+      var xOffset = (doc.internal.pageSize.width / 2) - (doc.getStringUnitWidth(text) * fontSize / 2)
+      doc.text(text, xOffset, 60)
       doc.setFontSize(10)
-      doc.autoTable(columns, rows)
+      doc.autoTable(columns, rows, {
+        margin: {top: 100}
+      })
       doc.save(pdfName + '.pdf')
+    },
+    printPDF () {
+      var pdfName = 'test'
+      var printDoc = new JsPDF()
+      var width = 105
+      var source = window.document.getElementById('pdf')
+      printDoc.fromHTML(source, 0, 0, {'width': width})
+      printDoc.save(pdfName + '.pdf')
     }
   }
 }
